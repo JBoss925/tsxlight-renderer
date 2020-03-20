9d720c91fd4400c61907f4c3f7b769d007d4f1ce
Jagger Brulato <jb2375@cornell.edu>
2020-03-19 20:57:41 -0400

    i may be stupid lol

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 // The entry point of the app!
 
-// Declare dummy namespace for heroku
-require('../framework/namespace/declareNamespace');
-
 function main() {
 
   // TODO: Update mainClass to your app's main class!
